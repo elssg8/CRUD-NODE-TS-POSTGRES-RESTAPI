f0800ecb0fa7b5d68734e524792315c35cf66b05
elssg8 <egarciac028@alumno.uaemex.mx>
2023-07-18 22:59:40 -0600

    a<b deleted :c

--- elss.java
+++
@@ -1,9 +0,0 @@
-public class elss {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        if (a < b) {
-            System.out.println("a is less than b");
-        }
-    }
-}
